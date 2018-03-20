19f97ff82f1736fcac75f9ad2add4f3d1e6e2e9a
Hunter Caron <huntercaron@mac.com>
2018-03-20 10:24:10 -0400

    scraping one by one

--- app.js
+++ app.js
@@ -1,37 +1,78 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const index = 10;
 
 (async () => {
   const browser = await puppeteer.launch({ executablePath: '../Chrome/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'});
   const page = await browser.newPage();
-  await page.goto('http://www.ted.com/talks/view/id/1', { waitUntil: 'networkidle0'});
-
+  await page.goto(`http://www.ted.com/talks/view/id/${index}`, { waitUntil: 'networkidle0'});
   const url = await page.url();
-  console.log(url);
-  
-  const transcriptSelector = '.Grid .Grid--with-gutter';
 
+  // save the URL
+
+
+  await page.goto(url + "/details")
+  const speakerSelector = ".sa";
+  await page.waitForSelector(speakerSelector)
+  const speakerInfo = await page.evaluate(speakerSelector => {
+    const speakerNode = document.querySelector(speakerSelector);
+    return {
+      speakerLink: speakerNode.href,
+      speakerName: speakerNode.firstChild.lastChild.firstChild.firstChild.textContent,
+      speakerTitle: speakerNode.firstChild.lastChild.firstChild.lastChild.lastChild.textContent,
+      speakerBio: speakerNode.lastChild.textContent,
+    }
+  }, speakerSelector)
+
+
+  const transcriptSelector = '.Grid .Grid--with-gutter';
   await page.goto(url + "/transcript")
   await page.waitForSelector(transcriptSelector)
-  // await page.waitForFunction("parseInt(document.querySelectorAll('.Grid .Grid--with-gutter')[10].children[0].textContent) > 0")
-  // 
-  // await page.screenshot({path: 'example.png', fullPage: true});
+
+  const transcript = {
+    index: index,
+    speakerInfo: speakerInfo,
+    transcript: [],
+  }
+
   
   const links = await page.evaluate(transcriptSelector => {
     const anchors = Array.from(document.querySelectorAll(transcriptSelector));
     return anchors.map(anchor => {
-      console.log(anchor.children);
-      
       return {
         time: anchor.children[0].textContent,
         text: anchor.children[1].textContent
       };
-      // const title = anchor.textContent.split('|')[0].trim();
-      // return `${title} - ${anchor.href}`;
     });
   }, transcriptSelector);
 
-  const h = links;
+  transcript.transcript = links;
   
+  // fs.writeFile('data.json', JSON.stringify([transcript]), 'utf8', () => {});
+
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      let obj = JSON.parse(data);
+      
+      let foundIndex = obj.findIndex((ele) => {
+        return ele.index == index;
+      });
+
+      if (foundIndex >= 0) {
+        obj[foundIndex] = transcript;
+      } else {
+        obj.push(transcript);
+      }
+      
+      json = JSON.stringify(obj);
+      fs.writeFile('data.json', json, 'utf8', () => {
+        console.log("Wrote to file");
+      }); // write it back 
+    }
+  });
 
   await browser.close();
 })();
