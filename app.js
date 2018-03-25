b6357e1d575071e51e8f0e71637d42c22cf4675e
Hunter Caron <huntercaron@mac.com>
2018-03-25 01:22:52 -0400

    writing file with async now

--- app.js
+++ app.js
@@ -1,69 +1,76 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const util = require('util');
 
-async function scrapeTed(index) {
-  const browser = await puppeteer.launch({ 
-    executablePath: '../Chrome/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
-    timeout: 100000,
-    args: ['--disable-timeouts-for-profiling']
-    });
-  const page = await browser.newPage();
-  await page.goto(`http://www.ted.com/talks/view/id/${index}`, { waitUntil: 'networkidle0'});
-  const url = await page.url();
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
-  await page.goto(url + "/details")
-  const speakerSelector = ".sa";
-  await page.waitForSelector(speakerSelector)
-  const speakerInfo = await page.evaluate(speakerSelector => {
-    const speakerNode = document.querySelector(speakerSelector);
-    return {
-      speakerLink: speakerNode.href,
-      speakerName: speakerNode.firstChild.lastChild.firstChild.firstChild.textContent,
-      speakerTitle: speakerNode.firstChild.lastChild.firstChild.lastChild.lastChild.textContent,
-      speakerBio: speakerNode.lastChild.textContent,
-    }
-  }, speakerSelector)
+const scrapeSize = 5;
 
+async function scrapeTed(index, browser) {
+  try {
+    const page = await browser.newPage();
+    await page.goto(`http://www.ted.com/talks/view/id/${index}`, { waitUntil: 'networkidle0'});
+    const url = await page.url();
 
-  const transcriptSelector = '.Grid .Grid--with-gutter';
-  await page.goto(url + "/transcript")
-  await page.waitForSelector(transcriptSelector)
-
-  const talk = {
-    index: index,
-    speakerInfo: speakerInfo,
-    transcript: [],
-  }
-
-  
-  const links = await page.evaluate(transcriptSelector => {
-    const anchors = Array.from(document.querySelectorAll(transcriptSelector));
-    return anchors.map(anchor => {
+    await page.goto(url + "/details")
+    const speakerSelector = ".sa";
+    await page.waitForSelector(speakerSelector)
+    const speakerInfo = await page.evaluate(speakerSelector => {
+      const speakerNode = document.querySelector(speakerSelector);
       return {
-        time: anchor.children[0].textContent,
-        text: anchor.children[1].textContent
-      };
-    });
-  }, transcriptSelector);
+        speakerLink: speakerNode.href,
+        speakerName: speakerNode.firstChild.lastChild.firstChild.firstChild.textContent,
+        speakerTitle: speakerNode.firstChild.lastChild.firstChild.lastChild.lastChild.textContent,
+        speakerBio: speakerNode.lastChild.textContent,
+      }
+    }, speakerSelector)
 
-  talk.transcript = links;
+
+    const transcriptSelector = '.Grid .Grid--with-gutter';
+    await page.goto(url + "/transcript")
+    await page.waitForSelector(transcriptSelector)
+
+    const talk = {
+      index: index,
+      speakerInfo: speakerInfo,
+      transcript: [],
+    }
+
+    
+    const links = await page.evaluate(transcriptSelector => {
+      const anchors = Array.from(document.querySelectorAll(transcriptSelector));
+      return anchors.map(anchor => {
+        return {
+          time: anchor.children[0].textContent,
+          text: anchor.children[1].textContent
+        };
+      });
+    }, transcriptSelector);
+
+    talk.transcript = links;
+    
+    // fs.writeFile('data.json', JSON.stringify([transcript]), 'utf8', () => {});
+
+    await page.close();
+    console.log(talk.speakerInfo.speakerName)
+    return new Promise(resolve => { resolve(talk); });
+
+  } catch (e) {
+    console.log(e);
+    return new Promise(reject => { reject({}); });
+  }
   
-  // fs.writeFile('data.json', JSON.stringify([transcript]), 'utf8', () => {});
-
-  await browser.close();
-  console.log(talk.speakerInfo.speakerName)
-
-  return new Promise(resolve => { resolve(talk) });
 };
 
 
 const talkCount = 4;
 
-async function scrapeMultiple(startIndex) {
+async function scrapeMultiple(startIndex, browser) {
   let promises = [];
 
-  for (let i = startIndex; i <= startIndex+9; i++) {
-    promises.push(scrapeTed(i));
+  for (let i = startIndex; i <= startIndex+scrapeSize; i++) {
+    promises.push(scrapeTed(i, browser));
   }
 
   const allData = await Promise.all(promises);
@@ -74,24 +81,42 @@ async function scrapeMultiple(startIndex) {
 
 
 (async () => {
-  for (let i = 1; i <= 30; i+=9) {
+  for (let i = 1; i <= 21; i+=scrapeSize) {
+    const browser = await puppeteer.launch({
+      executablePath: '../Chrome/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary',
+      timeout: 100000,
+      args: ['--disable-timeouts-for-profiling']
+    });
+
     console.log("hey")
-    let allData = await scrapeMultiple(i);
+    let newData = await scrapeMultiple(i, browser);
     // console.log(allData);
+
+    await browser.close();
     
-    for (let talk of allData) {
+    // for (let talk of allData) {
+    //   console.log()
+    //   if (talk.hasOwnProperty('speakerInfo')) {
+    //     // console.log(talk.speakerInfo.speakerName);
+    //   } else {
+    //     console.log( talk);
+    //   }
       
-      console.log(talk);
-    }
+    // }
+
+    const data = await readFile('data.json', 'utf8');
+    const fileData = JSON.parse(data);
+    const json = JSON.stringify(fileData.concat(newData));
+    await writeFile('data.json', json, 'utf8');
+    console.log("Wrote to file!")
 
     // fs.readFile('data.json', 'utf8', (err, data) => {
     //   if (err) {
     //     console.log(err);
     //   } else {
     //     let obj = JSON.parse(data);
-    //     obj.push(allData);
 
-    //     json = JSON.stringify(obj);
+    //     json = JSON.stringify(obj.concat(allData));
     //     fs.writeFile('data.json', json, 'utf8', () => {
     //       console.log("Wrote to file");
     //     }); // write it back 
